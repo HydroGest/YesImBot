e20981938b6c44ca8cc08343a3e885580fe75d7b
HydroGest <2445691453@qq.com>
2024-07-25 18:41:18 +0800

    [Tweak] Move handleResponse and processUserContent to utils

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,25 @@ import {
     h,
     Random
 } from "koishi";
+
 import {
     genSysPrompt,
     ensurePromptFileExists,
     getFileNameFromUrl,
 } from "./utils/prompt";
+
 import {
     run
 } from "./utils/api-adapter";
+
 import {
     SendQueue
 } from "./utils/queue";
+
+import {
+    handleResponse,
+	processUserContent
+} from "./utils/content";
 
 export const name = "yesimbot";
 
@@ -139,86 +147,6 @@ export const Config: Schema < Config > = Schema.object({
 
 const sendQueue = new SendQueue();
 
-function handleResponse(APIType: string, input: any): string {
-    let res: string;
-    switch (APIType) {
-        case "OpenAI": {
-            res = input.choices[0].message.content;
-            break;
-        }
-        case "Custom URL": {
-            res = input.choices[0].message.content;
-            break;
-        }
-        case "Cloudflare": {
-            res = input.result.response;
-            break;
-        }
-        default: {
-            throw new Error(`不支持的 API 类型: ${APIType}`);
-        }
-    }
-    console.log(typeof res);
-    if (typeof res != "string") {
-        res = JSON.stringify(res);
-    }
-    res = res.replaceAll("```", " ");
-    res = res.replaceAll("json", " ");
-    const LLMResponse = JSON.parse(res);
-    if (LLMResponse.status != "success") {
-        throw new Error(`LLM provides unexpected response: ${res}`);
-    }
-    let finalResponse: string = "";
-    if (LLMResponse.select)
-        finalResponse += h("quote", {
-            id: LLMResponse.select,
-        });
-    finalResponse += (LLMResponse.finReply ? LLMResponse.finReply : LLMResponse.reply);
-    return finalResponse;
-}
-
-async function processUserContent(session: any): Promise<string> {  
-    const regex = /<at id="([^"]+)"\s*\/>/g;  
-    // 转码 <at> 消息  
-    const matches = Array.from(session.content.matchAll(regex));  
-
-    const userContentPromises = matches.map(async (match) => {  
-        const id = match[1].trim();  
-        try {  
-            const user = await session.bot.getUser(id);  
-            return {  
-                match: match[0],  
-                replacement: `@${user.name}`,  
-            };  
-        } catch (error) {  
-            // QQ 官方机器人接口无法使用 session.bot.getUser()，尝试调用备用 API
-            try {  
-                const response = await fetch(`https://api.usuuu.com/qq/${id}`);  
-                if (!response.ok) {  
-                    throw new Error(`Failed to fetch user from backup API`);  
-                }  
-                const user = await response.json();  
-                return {  
-                    match: match[0],  
-                    replacement: `@${user.data.name || 'UserNotFound'}`,  
-                };  
-            } catch (backupError) {
-                return { match: match[0], replacement: `@UserNotFound` };  
-            }  
-        }  
-    });  
-
-    const userContents = await Promise.all(userContentPromises);  
-
-    let userContent: string = session.content;  
-
-    userContents.forEach(({ match, replacement }) => {  
-        userContent = userContent.replace(match, replacement);  
-    });  
-
-    return userContent;  
-}
-
 class APIStatus {
     private currentStatus: number = 0;
 
@@ -254,7 +182,7 @@ export function apply(ctx: Context, config: Config) {
             config.Group.Filter
         );
 
-        // 检测是否达到发送次数或被at
+        // 检测是否达到发送次数或被 at
         // 返回 false 的条件：
         // 发送队列已满 或者 用户消息提及机器人且随机条件命中：
         if (!sendQueue.checkQueueSize(groupId, config.Group.SendQueueSize) && !(
