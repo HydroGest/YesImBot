b6b234c4254c66925ece3c461f6a69324b276190
HydroGest <2445691453@qq.com>
2024-08-11 11:12:12 +0800

    [Fix] Fix at-tag name property bug

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -46,20 +46,22 @@ export function handleResponse(APIType: string, input: any, AllowErrorFormat: bo
 }
 
 export async function processUserContent(session: any): Promise<string> {  
-    const regex = /<at id="([^"]+)"\s*\/>/g;  
+    const regex = /<at id="([^"]+)"(?:\s+name="([^"]+)")?\s*\/>/g;  
     // 转码 <at> 消息  
     const matches = Array.from(session.content.matchAll(regex));  
 
     const userContentPromises = matches.map(async (match) => {  
         const id = match[1].trim();  
+        const name = match[2]?.trim(); // 可能获取到 name  
+
         try {  
             const user = await session.bot.getUser(id);  
             return {  
                 match: match[0],  
-                replacement: `@${user.name}`,  
+                replacement: `@${name || user.name}`, // 如果 name 存在，则使用它，否则使用 user.name  
             };  
         } catch (error) {  
-            // QQ 官方机器人接口无法使用 session.bot.getUser()，尝试调用备用 API
+            // QQ 官方机器人接口无法使用 session.bot.getUser()，尝试调用备用 API  
             try {  
                 const response = await fetch(`https://api.usuuu.com/qq/${id}`);  
                 if (!response.ok) {  
@@ -68,9 +70,9 @@ export async function processUserContent(session: any): Promise<string> {
                 const user = await response.json();  
                 return {  
                     match: match[0],  
-                    replacement: `@${user.data.name || 'UserNotFound'}`,  
+                    replacement: `@${name || user.data.name || 'UserNotFound'}`, // 使用 name 或备用 API 返回的名字  
                 };  
-            } catch (backupError) {
+            } catch (backupError) {  
                 return { match: match[0], replacement: `@UserNotFound` };  
             }  
         }  
