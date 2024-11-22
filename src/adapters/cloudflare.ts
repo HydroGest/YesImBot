507df2d6a806757582a701b30e6e398c64f68031
Miaow233 <miaow233@qq.com>
2024-11-22 12:58:42 +0800

    [fix] Cloudflare 请求

--- src/adapters/cloudflare.ts
+++ src/adapters/cloudflare.ts
@@ -1,15 +1,18 @@
+import { sendRequest } from "../utils/tools";
 import { BaseAdapter } from "./base";
 
 export class CloudflareAdapter extends BaseAdapter {
+  private url: string;
   private apiKey: string;
   private uid: string;
-  private url: string;
+  private model: string;
 
-  constructor(baseUrl: string, apiKey: string, uid: string, model :string) {
+  constructor(baseUrl: string, apiKey: string, uid: string, model: string) {
     super("Cloudflare");
     this.url = `${baseUrl}/accounts/${uid}/ai/run/${model}`;
     this.apiKey = apiKey;
     this.uid = uid;
+    this.model = model;
   }
 
   protected async generateResponse(
@@ -20,10 +23,18 @@ export class CloudflareAdapter extends BaseAdapter {
     eyeType: string,
     debug: boolean
   ) {
-
-    throw new Error("Not implemented");
     const requestBody = {
+      model: this.model,
       messages: await this.createMessages(sysPrompt, userPrompt, eyeType, detail),
+      temperature: parameters.Temperature,
+      max_tokens: parameters.MaxTokens,
+      top_p: parameters.TopP,
+      frequency_penalty: parameters.FrequencyPenalty,
+      presence_penalty: parameters.PresencePenalty,
+      stop: parameters.Stop,
+      ...parameters.OtherParameters,
     };
+    
+    return sendRequest(this.url, this.apiKey, requestBody, debug);
   }
 }
