3cb2c1f1f6a7b4c5c48e291aa3c5ec4732faf26c
MiaowFISH <neko-project@outlook.com>
2024-12-03 21:48:40 +0800

    限制获取到的消息数量

--- src/managers/queueManager.ts
+++ src/managers/queueManager.ts
@@ -13,20 +13,25 @@ export class QueueManager {
     this.ctx = ctx;
   }
 
-  async getQueue(channelId: string): Promise<ChatMessage[]> {
+  async getQueue(channelId: string, limit: number): Promise<ChatMessage[]> {
     return this.ctx.database
-      .select(DATABASE_NAME, {
-        channelId,
-      })
+      .select(DATABASE_NAME)
+      .where({ channelId })
       .orderBy("sendTime", "asc") // 升序
+      .limit(limit)
       .execute();
   }
 
-  async getMixedQueue(channels: Set<string>): Promise<ChatMessage[]> {
+  async getMixedQueue(
+    channels: Set<string>,
+    limit: number
+  ): Promise<ChatMessage[]> {
     const selectQuery = (channelIdFilter: any) => {
       return this.ctx.database
-        .select(DATABASE_NAME, { channelId: channelIdFilter })
+        .select(DATABASE_NAME)
+        .where({ channelId: channelIdFilter })
         .orderBy("sendTime", "asc") // 升序
+        .limit(limit)
         .execute();
     };
 
@@ -46,134 +51,23 @@ export class QueueManager {
     await this.ctx.database.create(DATABASE_NAME, chatMessage);
   }
 
-  public clearBySenderId(sender_id: string): boolean {
-    let hasCleared = false;
-    for (const [group, messages] of this.cacheManager.entries()) {
-      if (!group.startsWith("private:")) {
-        const originalLength = messages.length;
-        const filteredMessages = messages.filter(
-          (msg) => msg.sender_id !== sender_id
-        );
-        if (filteredMessages.length < originalLength) {
-          this.setQueue(group, filteredMessages);
-          hasCleared = true;
-        }
-      }
-    }
-    const privatePrefix = `private:${sender_id}`;
-    for (const group of this.cacheManager.keys()) {
-      if (group.startsWith(privatePrefix)) {
-        this.cacheManager.remove(group);
-        hasCleared = true;
-      }
-    }
-    return hasCleared;
+  public async clearBySenderId(senderId: string): Promise<boolean> {
+    const result = await this.ctx.database.remove(DATABASE_NAME, {
+      senderId,
+    });
+
+    return result.removed > 0;
   }
 
-  public getGroupKeys(): string[] {
-    return Array.from(this.cacheManager.keys());
-  }
-
-  public findGroupByMessageId(
-    messageId: string,
-    groups: Set<string>
-  ): string | null {
-    if (messageId.trim() === "") {
+  public async findChannelByMessageId(
+    messageId: string
+  ): Promise<string> {
+    const messages = await this.ctx.database
+    .select(DATABASE_NAME)
+    .where({ messageId })
+    .execute()
+    if (messages.length==0)
       return null;
-    }
-    for (const group of groups) {
-      if (this.cacheManager.has(group)) {
-        const queue = this.getQueue(group);
-        for (const message of queue) {
-          if (message.id === messageId) {
-            return group;
-          }
-        }
-      }
-    }
-    return null;
-  }
-
-  public resetQueue(group: string, maxQueueSize: number): void {
-    const queue = this.getQueue(group);
-    if (queue && queue.length > 0) {
-      const newQueue = queue.slice(-maxQueueSize);
-      this.setQueue(group, newQueue);
-      console.log(`此会话队列已满，已出队至 ${newQueue.length} 条`);
-    }
-  }
-
-  public clearQueue(group: string): boolean {
-    if (this.cacheManager.has(group)) {
-      this.cacheManager.remove(group);
-      console.log(`已清空此会话: ${group}`);
-      return true;
-    } else {
-      console.log(`此会话不存在: ${group}`);
-      return false;
-    }
-  }
-}
-
-export class TriggerManager {
-  private triggerCountMap: Map<string, number>;
-  private lastTriggerTimeMap: Map<string, number>;
-  readonly maxTriggerTime: number;
-
-  constructor(private config: Config) {
-    this.triggerCountMap = new Map();
-    this.lastTriggerTimeMap = new Map();
-    this.maxTriggerTime = config.MemorySlot.MaxTriggerTime * 1000 || 2147483647;
-  }
-
-  public getTriggerCount(group: string): number {
-    return this.triggerCountMap.get(group) || 0;
-  }
-
-  public setTriggerCount(group: string, count: number): void {
-    this.triggerCountMap.set(group, count);
-  }
-
-  public resetTriggerCount(group: string, nextTriggerCount: number): void {
-    this.setTriggerCount(group, nextTriggerCount);
-  }
-
-  public checkTrigger(group: string): boolean {
-    const count = this.getTriggerCount(group);
-    return count <= 0;
-  }
-
-  public getLastTriggerTime(group: string): number {
-    return this.lastTriggerTimeMap.get(group) || 0;
-  }
-
-  public updateLastTriggerTime(group: string): void {
-    this.lastTriggerTimeMap.set(group, Date.now());
-  }
-}
-
-export class QuietTimerManager {
-  private quietTimeoutMap: Map<string, NodeJS.Timeout>;
-
-  constructor() {
-    this.quietTimeoutMap = new Map();
-  }
-
-  public startTimer(
-    groupId: string,
-    delay: number,
-    callback: () => void
-  ): void {
-    this.clearTimer(groupId);
-    const timeout = setTimeout(callback, delay);
-    this.quietTimeoutMap.set(groupId, timeout);
-  }
-
-  public clearTimer(groupId: string): void {
-    const existingTimeout = this.quietTimeoutMap.get(groupId);
-    if (existingTimeout) {
-      clearTimeout(existingTimeout);
-      this.quietTimeoutMap.delete(groupId);
-    }
+    return messages[0].channelId;
   }
 }
--- src/services/sendQueue.ts
+++ src/services/sendQueue.ts
@@ -19,26 +19,26 @@ export class SendQueue {
     this.queueManager = new QueueManager(ctx);
   }
   async checkQueueSize(channelId: string): Promise<boolean> {
-    return (await this.queueManager.getQueue(channelId)).length > this.slotSize;
+    return (await this.queueManager.getQueue(channelId, this.slotSize)).length > this.slotSize;
   }
 
   async checkMixedQueueSize(channelId: string): Promise<boolean> {
     for (let slotContain of this.slotContains) {
       if (slotContain.has(channelId)) {
-        return (await this.queueManager.getMixedQueue(slotContain)).length > this.slotSize;
+        return (await this.queueManager.getMixedQueue(slotContain, this.slotSize)).length > this.slotSize;
       }
     }
     return false;
   }
 
-  async getQueue(channelId: string): Promise<ChatMessage[]> {
-    return this.queueManager.getQueue(channelId);
+  async getQueue(channelId: string, count: number): Promise<ChatMessage[]> {
+    return this.queueManager.getQueue(channelId, count);
   }
 
   async getMixedQueue(channelId: string): Promise<ChatMessage[]>{
     for (let slotContain of this.slotContains) {
         if (slotContain.has(channelId)) {
-          return await this.queueManager.getMixedQueue(slotContain);
+          return await this.queueManager.getMixedQueue(slotContain, this.slotSize);
         }
       }
       return [];
